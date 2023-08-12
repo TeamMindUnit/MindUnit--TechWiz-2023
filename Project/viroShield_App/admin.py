e66ceb4e4b402c3035bd8fd7e1c649a4d3878e63
Ahmed Hamza <ahmedarif2882@gmail.com>
2023-08-12 11:47:40 +0500

    Admin UI updated

--- Project/viroShield_App/admin.py
+++ Project/viroShield_App/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models.patients import Patient
 from .models.predictions import Prediction
 from .models.healthstatus import HealthStatus
-# Register your models here.
 
 admin.site.register(Patient)
 admin.site.register(Prediction)
